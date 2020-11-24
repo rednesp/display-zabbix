9a9cf4b20edf391edd6d105d3013b67908f77732
Antonio Francisco <ajoaoff@gmail.com>
2020-11-24 10:24:51 -0300

    Classes to control activation/deactivation
    Activate method activate is called when the plugin
    is activated. Deactivate method deactivate is called when
    the plugin is deactivated

--- display-zabbix.php
+++ display-zabbix.php
@@ -14,3 +14,14 @@ defined('ABSPATH') or die('Get out!');
 if ( file_exists( dirname(__FILE__).'/vendor/autoload.php')) {
     require_once dirname(__FILE__).'/vendor/autoload.php';
 }
+
+
+function activate_display_zabbix_plugin() {
+    Includes\Base\Activate::activate();
+}
+register_activation_hook(__FILE__, 'activate_display_zabbix_plugin');
+
+function deactivate_display_zabbix_plugin() {
+    Includes\Base\Deactivate::deactivate();
+}
+register_deactivation_hook(__FILE__, 'deactivate_display_zabbix_plugin');
