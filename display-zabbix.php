73f01cecec3c013bdc94ba4d7eb915a3fbafe2a9
Antonio Francisco <ajoaoff@gmail.com>
2020-11-24 10:22:55 -0300

    Use composer

--- display-zabbix.php
+++ display-zabbix.php
@@ -1,6 +1,16 @@
+<?php
+/**
+ * @package DisplayZabbix
+ */
 /**
  * Plugin Name: Display Zabbix Graphs
  * Description: Show Zabbix Graphs inside Wordpress
  * Author: Antonio Francisco
  * Version: 0.1
  */
+
+defined('ABSPATH') or die('Get out!');
+
+if ( file_exists( dirname(__FILE__).'/vendor/autoload.php')) {
+    require_once dirname(__FILE__).'/vendor/autoload.php';
+}
